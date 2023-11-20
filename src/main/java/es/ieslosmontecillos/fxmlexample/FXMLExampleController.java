b8930b989ac4d97c6957c42153047d7f7826c948
Alejandro Fernández Barrionuevo <alejandrofernandezbarrionuevo@gmail.com>
2023-11-20 18:29:09 +0100

    FXMLExample - Version 2.0 (Added username and password info)

--- src/main/java/es/ieslosmontecillos/fxmlexample/FXMLExampleController.java
+++ src/main/java/es/ieslosmontecillos/fxmlexample/FXMLExampleController.java
@@ -2,13 +2,21 @@ package es.ieslosmontecillos.fxmlexample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
 public class FXMLExampleController {
     @FXML private Text actiontarget;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private TextField usernameField;
 
-    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
-        actiontarget.setText("Sign in button pressed");
+    @FXML
+    protected void handleSubmitButtonAction(ActionEvent event)
+    {
+        actiontarget.setText("Sign in button pressed\nUser Name: " + usernameField.getText() + "\nPassword: " + passwordField.getText());
     }
 
 }
